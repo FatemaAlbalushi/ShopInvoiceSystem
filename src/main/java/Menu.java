ffaadc810ed2775ef28f5644e55323112e721c89
FatemaAlbalushi <fatoomsaid188@gmail.com>
2023-01-30 20:25:29 +0400

    updated Menu file

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,36 +1,60 @@
 import java.util.ArrayList;
 
 /**
- * This is the menu class that allows the user to add the menu 
+ * This is the menu class that stores and displays the menu items
  *
  */
 public class Menu {
-	  String title;
-	  ArrayList<MenuItem>listOfMenuItems =  new ArrayList<MenuItem>();
-	
-	public Menu() {
-//		this.title= title;
-//		listOfMenuItems.add(menuItem);
+	String title; // Title of the menu
+	ArrayList<MenuItem> listOfMenuItems; // ArrayList to store the menu items
+
+	/**
+	 * Returns the title of the menu.
+	 * 
+	 * @return the title of the menu
+	 */
+	public String getTitle() {
+		return title;
 	}
-	
+
+	/**
+	 * Sets the title of the menu.
+	 * 
+	 * @param title the title of the menu
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * Method to add menu item to the ArrayList
+	 * 
+	 * @param menuItem
+	 */
 	public void addMenuitem(MenuItem menuItem) {
-		listOfMenuItems.add(menuItem);	
+		listOfMenuItems.add(menuItem);
 	}
-	
-	
+
+	/**
+	 * Method to get a menu item based on its id
+	 * 
+	 * @param id
+	 * @return
+	 */
 	public MenuItem getMenuItem(int id) {
-		return listOfMenuItems.get(id-1);
+		return listOfMenuItems.get(id - 1);
 	}
-	
+
+	/**
+	 * Method to print the menu items
+	 */
 	public void printMenuitem() {
-		
-		if (title!=null) {
+
+		if (title != null) { // Print the title if it is not null
 			System.out.println(title);
 		}
-		for(MenuItem currItem: listOfMenuItems) {
-			System.out.println(currItem.id+" : "+ currItem.desc);
+		for (MenuItem currItem : listOfMenuItems) { // Loop through the menu items and print
+			System.out.println(currItem.id + " : " + currItem.desc);
 		}
-		
 	}
 }
- 
