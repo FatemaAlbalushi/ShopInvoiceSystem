2f3bab707ca8fd43766c568a715ec7fcd6debb12
FatemaAlbalushi <fatoomsaid188@gmail.com>
2023-01-30 12:40:54 +0400

    updated file

--- src/main/java/InvoiceSystemMain.java
+++ src/main/java/InvoiceSystemMain.java
@@ -1,3 +1,4 @@
+import java.lang.StackWalker.Option;
 import java.util.Scanner;
 
 
@@ -28,11 +29,11 @@ public class InvoiceSystemMain {
 		subMenuAdminAction2.addMenuitem(new MenuItem(1, "Add Items"));
 		subMenuAdminAction2.addMenuitem(new MenuItem(2, "Delete Items"));
 		subMenuAdminAction2.addMenuitem(new MenuItem(3, "Change Item Price"));
-		subMenuAdminAction2.addMenuitem(new MenuItem(3, "Report All Items"));
-		subMenuAdminAction2.addMenuitem(new MenuItem(4, "Go Back"));
+		subMenuAdminAction2.addMenuitem(new MenuItem(4, "Report All Items"));
+		subMenuAdminAction2.addMenuitem(new MenuItem(5, "Go Back"));
 		
 		MenuItem adminActionMenu1= new MenuItem(1, "Shop Settings");
-		adminActionMenu1.marktheItemAsMenu(subMenuAdminAction2);
+		adminActionMenu1.marktheItemAsMenu(subMenuAdminAction1);
 		
 		MenuItem adminActionMenu2= new MenuItem(2, "Manage Shop Items");
 		adminActionMenu2.marktheItemAsMenu(subMenuAdminAction2);
@@ -47,75 +48,99 @@ public class InvoiceSystemMain {
         patentMenu.addMenuitem(new MenuItem(6, "Search for Invoice"));
         patentMenu.addMenuitem(new MenuItem(7, "Program Statistics "));
         patentMenu.addMenuitem(new MenuItem(8, "Exit"));
-        patentMenu.printMenuitem();
+        //patentMenu.printMenuitem();
         
-        
+        Integer choice;
+        Integer option;
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Please Enter Your choice: ");
-        Integer choice = Integer.parseInt(userInput.nextLine());
-        
-//        while (choice == null) {
-//            System.out.println("Enter your choice: ");
-//            try {
-//                choice = Integer.parseInt(userInput.nextLine());
-//            } catch (NumberFormatException e) {
-//                System.out.println("Invalid input. Please enter a valid integer.");
-//            }
-//        }
-        
-        
-        
-        if (choice==1) {
-        	
-			patentMenu.getMenuItem(1).menu.printMenuitem();
-			
-		}
-        
-        else if (choice==2) {
-        	patentMenu.getMenuItem(2).menu.printMenuitem();
-        	Scanner sc = new Scanner(System.in);
-            int option;
-			
-			do {
-		        option = sc.nextInt();
-		        switch (option) {
+        do {
+        	patentMenu.printMenuitem();
+            System.out.print("Enter your Choice : ");
+            choice = Integer.parseInt(userInput.nextLine());
+            switch (choice) {
+            case 1:
+                do {
+                    System.out.println("Shop Settings");
+                    patentMenu.getMenuItem(1).menu.printMenuitem();
+                    System.out.print("Enter your Choice : ");
+                    option = Integer.parseInt(userInput.nextLine());
+                    switch (option) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        System.out.println("Going Back...");
+                        break;
+                    default:
+                        System.out.println("Invalid option!!!");
+                        break;
+                    }
+                } while (option != 4);
+                break;
+            case 2:
+                do {
+                    System.out.println("Shop Settings");
+                    patentMenu.getMenuItem(2).menu.printMenuitem();
+                    System.out.print("Enter your Choice : ");
+                    option = Integer.parseInt(userInput.nextLine());
+                    switch (option) {
 		            case 1:
-		            	System.out.println("Enter item id: ");
-		                int itemId = sc.nextInt();
-		                System.out.println("Enter item name: ");
-		                String itemName = sc.next();
-		                System.out.println("Enter item price: ");
-		                double itemPrice = sc.nextDouble();
-		                System.out.println("Enter item quantity: ");
-		                int itemQuantity = sc.nextInt();
-		              //  item.addItem(itemId, itemName, itemPrice, itemQuantity);
-		            	//ShopItem.addItem(item);
-		                break;
-		            case 2:
-		            //	ShopItem.deleteItem();
-		                break;
-		            case 3:
-		            //	ShopItem.changeItemPrice();
-		                break;
-		            case 4:
-		            	//ShopItem.reportAllItems();
-		                break;
-		            case 5:
-		                break;
-		            default:
-		                System.out.println("Invalid option");
-		                break;
-		        }
-		    } while (option != 5);
-			
-		}
-        
-        
-        
-        System.out.println("Application Ended");
+	            	System.out.println("Enter item id: ");
+	                int itemId = userInput.nextInt();
+	                System.out.println("Enter item name: ");
+	                String itemName = userInput.next();
+	                System.out.println("Enter item price: ");
+	                double itemPrice = userInput.nextDouble();
+	                System.out.println("Enter item quantity: ");
+	                int itemQuantity = userInput.nextInt();
+	              //  item.addItem(itemId, itemName, itemPrice, itemQuantity);
+	            	//ShopItem.addItem(item);
+	                break;
+	            case 2:
+	            //	ShopItem.deleteItem();
+	                break;
+	            case 3:
+	            //	ShopItem.changeItemPrice();
+	                break;
+	            case 4:
+	            	//ShopItem.reportAllItems();
+	                break;
+                    case 5:
+                        System.out.println("Going Back...");
+                        break;
+                    default:
+                        System.out.println("Invalid option!!");
+                        break;
+                    }
+                } while (option != 5);
+                break;
+            case 3:
+                System.out.println("Create New Invoice");
+                break;
+            case 4:
+                System.out.println("Report - Statistics");
+                break;
+            case 5:
+                System.out.println("Report - All Invoices");
+                break;
+            case 6:
+                System.out.println("Search Invoice");
+                break;
+            case 7:
+                System.out.println("Program Statistics");
+                break;
+            case 8:
+                System.out.println("EXITING PROGRAM...");
+                break;
+            default:
+                System.out.println("Invalid option!!!");
+                break;
+            }
+        } while (choice != 8);
+ 
 	}
-
-	
-	
 }
 
