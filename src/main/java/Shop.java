f60e16ac6342064554d88008a0423d31b1bc0351
FatemaAlbalushi <fatoomsaid188@gmail.com>
2023-01-31 12:47:10 +0400

    updated Shop file

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -14,16 +14,32 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 
+/**
+ * The Shop class represents a shop object with a name, phone number, fax number, email and website.
+ * It contains an ArrayList of ShopItems and Invoices.
+ * There are two constructors available: one with 5 parameters to initialize all member variables
+ * and one with only 1 parameter to only initialize the shop name.
+ * The class also provides setters for the phone number, fax number, email and website.
+ */
+
 public class Shop {
-	String Shopname;
-	private String tel;
-	private String fax;
-	private String email;
-	private String website;
+	String Shopname; // Name of the shop
+	private String tel; // Telephone number of the shop
+	private String fax; // Fax number of the shop
+	private String email; // Email address of the shop
+	private String website; // Website of the shop
 
-	private ArrayList<ShopItem> shopitemlist = new ArrayList<ShopItem>();
-	private ArrayList<Invoice> invoices = new ArrayList<Invoice>();
+	private ArrayList<ShopItem> shopitemlist = new ArrayList<ShopItem>(); // List of shop items
+	private ArrayList<Invoice> invoices = new ArrayList<Invoice>(); // List of invoices
 
+	/**
+	 * Constructor for creating a Shop object with all contact information.
+	 * @param shopname Name of the shop.
+	 * @param tel Telephone number of the shop.
+	 * @param fax Fax number of the shop.
+	 * @param email Email address of the shop.
+	 * @param website Website of the shop.
+	 */
 	public Shop(String shopname, String tel, String fax, String email, String website) {
 		this.Shopname = shopname;
 		this.tel = tel;
@@ -32,29 +48,63 @@ public class Shop {
 		this.website = website;
 	}
 
+	/**
+	 * Constructor for creating a Shop object with only the name.
+	 * @param shopname Name of the shop.
+	 */
 	public Shop(String shopname) {
 		this.Shopname = shopname;
 	}
 
+	/**
+	 * Setter method for setting the telephone number of the shop.
+	 * @param tel Telephone number of the shop.
+	 */
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
 
+	/**
+	 * Setter method for setting the fax number of the shop.
+	 * @param fax Fax number of the shop.
+	 */
 	public void setFax(String fax) {
 		this.fax = fax;
 	}
 
+	/**
+	 * Setter method for setting the email address of the shop.
+	 * @param email Email address of the shop.
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * Setter method for setting the website of the shop.
+	 * @param website Website of the shop.
+	 */
 	public void setWebsite(String website) {
 		this.website = website;
 	}
 
-	public void SetShopName() {
-		String newShopName = null;
+	
+	
+	/**
+	 * Method to change the name of the shop.
+	 * Prompts user to enter the new name for the shop.
+	 * Updates the shop name with the new name entered by the user
+	*/
+	public void setShopName() {
+		//Prompt the user to enter a new shop name
 		System.out.print("Enter the new shop name: ");
+		
+		//Create a variable to hold the new shop name
+		String newShopName = null;
+		
+		//Get the input from the user and store it in the newShopName variable
+	
+		//Set the shop name of the object to the new shop name
 		this.Shopname = newShopName;
 	}
 	
@@ -77,56 +127,11 @@ public class Shop {
 	    }
 	}
 
-//	public  void addItem() {
-//	   final String itemsListFile = "data/items.json";
-//	        File itemsFile = new File(itemsListFile);
-//	        try {
-//	            if (!itemsFile.exists()) {
-//	                itemsFile.createNewFile();
-//	            }
-//	        } catch (IOException e) {
-//	            System.out.println("Error creating file: " + e.getMessage());
-//	            return;
-//	        }
-//		Scanner userInput = new Scanner(System.in);
-//		System.out.println("Enter item id: ");
-//		int itemId = userInput.nextInt();
-//
-//		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//		   Type type = new TypeToken<List<ShopItem>>(){}.getType();
-//		   List<ShopItem> items = new ArrayList<>();
-//
-//		   try (FileReader reader = new FileReader("data/items.json")) {
-//		      items = gson.fromJson(reader, type);
-//		   } catch (IOException e) {
-//		      e.printStackTrace();
-//		   }
-//
-//		   for (ShopItem item : items) {
-//		      if (item.getitemId() == itemId) {
-//		         System.out.println("Item with id " + itemId + " already exists.");
-//		         return;
-//		      }
-//		   }
-//		   
-//		System.out.println("Enter item name: ");
-//		String itemName = userInput.next();
-//
-//		System.out.println("Enter item price: ");
-//		double itemPrice = userInput.nextDouble();
-//
-//		System.out.println("Enter item quantity: ");
-//		int itemQuantity = userInput.nextInt();
-//		ShopItem shopItem1 = new ShopItem(itemId, itemName, itemPrice, itemQuantity);
-//		items.add(shopItem1);
-//
-//		try (FileWriter writer = new FileWriter("data/items.json")) {
-//			gson.toJson(items, writer);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//
-//	}
+	/**
+	 * Adds a new item to the list of items in the shop.
+	 *
+	 * The item details are read from the standard input (keyboard) and the updated list of items is saved to a file.
+	 */
 	public void addItem() {
 	    // Define the file name for the items list
 	    final String itemsListFile = "data/items.json";
@@ -206,52 +211,75 @@ public class Shop {
 
 
 	
-	
-	
+	/**
+	 * The method 'loadItems' is used to load the shop items from the JSON file 'data/items.json'.
+	 * The items are then displayed on the console with the following format:
+	 * Item ID Item Name Unit Price Quantity Qty Amount
+	 * The method uses the Gson library to parse the JSON file. A type is defined using the TypeToken class to indicate that the JSON file contains a list of ShopItem objects.
+	 * A try-with-resources block is used to open the reader for the JSON file, and the items are read and stored in a List.
+	 * The items are then displayed on the console with the specified format.
+	 * In case of an IOException, the stack trace is printed.
+	*/
 	public void loadItems() {
-			    Gson gson = new Gson();
-			    Type type = new TypeToken<List<ShopItem>>(){}.getType();
-			    try (FileReader reader = new FileReader("data/items.json")) {
-			      List<ShopItem> items = gson.fromJson(reader, type);
-			      System.out.println("Item ID\tItem Name\tUnit Price\tQuantity\tQty Amount");
-			      for (ShopItem item : items) {
-			        System.out.println(item.getitemId() + "\t" + item.getItemName() + "\t"+"\t" + item.getUnitPrice() + "\t" +"\t"+ item.getQuantity() + "\t"+ "\t"+ item.getQtyAmount());
-			      }
-			    } catch (IOException e) {
-			      e.printStackTrace();
-			    }	  
-	    
-	}
-	
-	
-	public void deleteItem(int itemId) {
-		  //Gson gson = new Gson();
-		  Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		  Type type = new TypeToken<List<ShopItem>>(){}.getType();
-		  try (FileReader reader = new FileReader("data/items.json")) {
-		    List<ShopItem> items = gson.fromJson(reader, type);
-		    List<ShopItem> itemsToSave = new ArrayList<>();
-		    boolean itemFound = false;
-		    for (ShopItem item : items) {
-		      if (item.getitemId() != itemId) {
-		        itemsToSave.add(item);
-		      } else {
-		        itemFound = true;
-		      }
-		    }
-		    if (!itemFound) {
-		      System.out.println("Item with id " + itemId + " was not found.");
-		      return;
-		    }
-		    try (FileWriter writer = new FileWriter("data/items.json")) {
-		      gson.toJson(itemsToSave, writer);
-		    }
-		  } catch (IOException e) {
-		    e.printStackTrace();
-		  }
-		}
+	    // Initialize the Gson object
+	    Gson gson = new Gson();
+	    // Define the type of object to be used for parsing
+	    Type type = new TypeToken<List<ShopItem>>(){}.getType();
+	    try (FileReader reader = new FileReader("data/items.json")) {
+	      // Read and parse the JSON data into a list of ShopItem objects
+	      List<ShopItem> items = gson.fromJson(reader, type);
+	      // Display the items details
+	      System.out.println("Item ID\tItem Name\tUnit Price\tQuantity\tQty Amount");
+	      for (ShopItem item : items) {
+	        System.out.println(item.getitemId() + "\t" + item.getItemName() + "\t"+"\t" + item.getUnitPrice() + "\t" +"\t"+ item.getQuantity() + "\t"+ "\t"+ item.getQtyAmount());
+	      }
+	    } catch (IOException e) {
+	      // Print the stack trace in case of any error
+	      e.printStackTrace();
+	    }	  
 
-	
+}
+	/**
+
+	Method to delete a ShopItem from the list of items stored in a JSON file.
+	@param itemId The ID of the item to be deleted.
+	*/
+	public void deleteItem(int itemId) {
+	Gson gson = new GsonBuilder().setPrettyPrinting().create(); // Gson instance with pretty printing enabled
+	Type type = new TypeToken<List<ShopItem>>(){}.getType(); // type parameter for the list of ShopItems
+	try (FileReader reader = new FileReader("data/items.json")) {
+	// Read the JSON file into a list of ShopItems
+	List<ShopItem> items = gson.fromJson(reader, type);
+	// Create a new list to store the items that will be saved
+	List<ShopItem> itemsToSave = new ArrayList<>();
+
+	// Flag to keep track of whether the item was found or not
+	boolean itemFound = false;
+
+	// Loop through the list of items and add the items that are not being deleted to the new list
+	for (ShopItem item : items) {
+	  if (item.getitemId() != itemId) {
+	    itemsToSave.add(item);
+	  } else {
+	    itemFound = true;
+	  }
+	}
+
+	// If the item was not found, display a message and return
+	if (!itemFound) {
+	  System.out.println("Item with id " + itemId + " was not found.");
+	  return;
+	}
+
+	// Write the updated list of items to the JSON file
+	try (FileWriter writer = new FileWriter("data/items.json")) {
+	  gson.toJson(itemsToSave, writer);
+	}
+	} catch (IOException e) {
+		e.printStackTrace();
+		}
+}
+
 
 	
 	public static void changeItemPrice(int itemId, double newPrice) {
